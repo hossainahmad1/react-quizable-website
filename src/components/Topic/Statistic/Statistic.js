7c58571e3ea8c969345cd777da6283c660c90b8e
Hossain Ahmad <hossain177922@gmail.com>
2022-10-12 18:40:58 +0600

    rechart

--- src/components/Topic/Statistic/Statistic.js
+++ src/components/Topic/Statistic/Statistic.js
@@ -8,8 +8,12 @@ const Statistic = () => {
     console.log(data)
     return (
         <div>
-            <LineChart width={500} height={400} data={data} >
+            <LineChart width={500} height={400} data={data} margin={{ top: 10, right: 20, bottom: 5, left: 10 }}>
                 <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <Tooltip />
             </LineChart>
         </div>
     );
